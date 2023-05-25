8f8086c89801484ec78f6f55a08dfb22b5ebe50b
Kuznetsova15 <113414850+Kuznetsova15@users.noreply.github.com>
2023-05-25 10:23:21 +0300

    Update index.php

--- index.php
+++ index.php
@@ -107,7 +107,7 @@ else {
     setcookie('email_error', '', 100000);
   }
   
-  if ($_POST['year']=='Год') {
+  if ($_POST['year']=='Год') {///////////////////////////////////////////////////////////////////////////////////
     setcookie('year_error', '1', time() + 24 * 60 * 60);
     setcookie('year_value', '', 100000);
     $errors = TRUE;
